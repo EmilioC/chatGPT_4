0001c4fce3653a67aa0579534cf80cba339ddec3
EmilioC <emicorgul@gmail.com>
2023-03-01 23:38:53 +0100

    Manage the input of busqueda.component

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   ]
 })
 export class BusquedaComponent {
+
+/*   ViewClild busca cualquier elemento del DOM y podemos
+  manejarlo 
+  El símbolo ! indica que not null accesion operator
+  es un operador para asegurarse que el objeto no es nulo
+  Al tipar textBuscar como HTMLInputElement tenemos acceso al .value*/
+  @ViewChild( 'txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar( ){
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(this.txtBuscar)
+    this.txtBuscar.nativeElement.value='';
+  }
 
 }
