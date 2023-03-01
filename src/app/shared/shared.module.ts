efdd6ca76e77ddfb888e6bd97c3aecc98124bf44
EmilioC <emicorgul@gmail.com>
2023-03-01 23:20:12 +0100

    Create components and manage in gifs.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
-
-
 @NgModule({
   declarations: [
     SidebarComponent
@@ -12,7 +10,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     CommonModule
   ],
   exports:[
-    SidebarComponent
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
